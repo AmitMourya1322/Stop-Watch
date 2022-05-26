4a6e282cd5b46cb9a9a950593f6a70655a0d52fd
AMIT KUMAR MOURYA <30823625+AmitMourya1322@users.noreply.github.com>
2022-05-26 12:07:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+// m = milliseconds
+// s = seconds
+// mt =minutes
+// h = hours
+
+let [m,s,mt,h] = [0,0,0,0];
+let timerRef = document.querySelector('.timeDisplay');
+let int = null;
+
+//start button event
+document.getElementById('start').addEventListener('click', ()=>{
+    if(int!==null){
+        clearInterval(int);
+    }
+    int = setInterval(displayTimer,10);
+});
+
+//pause button event
+document.getElementById('pause').addEventListener('click', ()=>{
+    clearInterval(int);
+});
+
+//reset button event
+document.getElementById('reset').addEventListener('click', ()=>{
+    clearInterval(int);
+    [m,s,mt,h] = [0,0,0,0];
+    timerRef.innerHTML = '00 : 00 : 00 : 000 ';
+});
+
+//function to display time
+function displayTimer(){
+    m+=10;
+    if(m == 1000){
+        m= 0;
+        s++;
+        if(s == 60){
+            s = 0;
+            mt++;
+            if(mt == 60){
+                mt = 0;
+                h++;
+            }
+        }
+    }
+    var hour = h< 10 ? "0" + h : h;
+    var minutes = mt < 10 ? "0" + mt : mt;
+    var seconds = s < 10 ? "0" + s : s;
+    var milliseconds = m < 10 ? "00" + m : m < 100 ? "0" + m: m;
+
+    timerRef.innerHTML = ` ${hour} : ${minutes} : ${seconds} : ${milliseconds}`;
+}
